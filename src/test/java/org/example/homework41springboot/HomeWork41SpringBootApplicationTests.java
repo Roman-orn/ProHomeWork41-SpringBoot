8c3c49821176f21da7ec30f0261ead7f015bc48a
Roman-orn <0671635566@ukr.net>
2026-09-06 11:05:20 +0300

    Initial commit

---
+++ src/test/java/org/example/homework41springboot/HomeWork41SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.homework41springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork41SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
